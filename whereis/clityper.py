bee739ada1b375fdb401dd5ee4e31b2ce3d39d40
ALinuxPerson <micheal02052007@gmail.com>
2020-07-12 16:12:01 +0800

    Fix reversed options and arguments

--- whereis/clityper.py
+++ whereis/clityper.py
@@ -7,8 +7,8 @@ app: typer.Typer = typer.Typer()
 
 @app.command()
 def find(
-    name: str,
-    database_location: Path = typer.Argument(
+    name: str = typer.Argument(..., help="The name of the entry."),
+    database_location: Path = typer.Option(
         utils.config_folder(), help="The location of the database."
     ),
 ) -> None:
@@ -17,13 +17,13 @@ def find(
 
 @app.command()
 def entry(
-    name: str,
-    database_location: Path = typer.Argument(
+    name: str = typer.Argument(..., help="The name of the entry."),
+    database_location: Path = typer.Option(
         utils.config_folder(), help="The location of the database."
     ),
-    info: bool = typer.Argument(True, help="Show information about the entry."),
-    add: bool = typer.Argument(False, help="Add the entry to the database."),
-    remove: bool = typer.Argument(False, help="Remove the entry from the database."),
+    info: bool = typer.Option(True, help="Show information about the entry."),
+    add: bool = typer.Option(False, help="Add the entry to the database."),
+    remove: bool = typer.Option(False, help="Remove the entry from the database."),
 ) -> None:
     """Query, add and remove the entry with the name NAME."""
 
